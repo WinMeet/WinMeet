9d9f4ee73053b3dea6060c3880312e785ba9da29
cem256 <cemo99@gmail.com>
2022-12-23 19:48:56 +0300

    router configured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./views/Home";
+import { Login } from "./views/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
