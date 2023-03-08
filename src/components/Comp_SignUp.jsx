6c6bbf43f631191f3bbc638f1211fbc40e7003cc
cem256 <cemo99@gmail.com>
2023-03-08 15:31:55 +0300

    signup form validation

--- src/components/Comp_SignUp.jsx
+++ src/components/Comp_SignUp.jsx
@@ -1,12 +1,48 @@
 import React from "react";
-import { useState } from "react";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { Password } from "primereact/password";
 
+import axios from "axios";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
 const CompSignUp = () => {
-  const [value, setValue] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Please enter a valid email")
+        .required("Email cannot be empty"),
+
+      password: Yup.string()
+        .required("Password cannot be empty")
+        .matches(
+          /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+          "Password must contain at least one uppercase letter and number, and be at least 8 characters long"
+        ),
+    }),
+
+    onSubmit: async (values) => {
+      try {
+        // TODO: Remove
+        alert(JSON.stringify(values, null, 2));
+        const response = await axios.post("localhost:8080/api/login", {
+          email: values.email,
+          password: values.password,
+        });
+        const data = await response.json();
+        alert(JSON.stringify(data, null, 2));
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
   return (
     <>
       <div>
@@ -33,67 +69,93 @@ const CompSignUp = () => {
               &nbsp;for free
             </div>
             <Card className="shadow-5">
-              <div>
-                <div className="">
-                  <InputText placeholder="E-mail" />
-                  <div className="pt-3">
-                    <Password
-                      placeholder="Password"
-                      value={value}
-                      onChange={(e) => setValue(e.target.value)}
+              <form onSubmit={formik.handleSubmit}>
+                <div>
+                  <div className="">
+                    <InputText
+                      placeholder="E-mail"
+                      name="email"
+                      type="text"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                    />
+                    <div>
+                      <div className="card flex justify-content-center pt-2 text-red-500 font-normal">
+                        {formik.touched.email && formik.errors.email ? (
+                          <div>{formik.errors.email}</div>
+                        ) : null}
+                      </div>
+                    </div>
+                    <div className="pt-3">
+                      <Password
+                        feedback={false}
+                        placeholder="Password"
+                        name="password"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                      />
+                      <div>
+                        <div className="card flex justify-content-center pt-2 text-red-500 font-normal">
+                          {formik.touched.password && formik.errors.password ? (
+                            <div>{formik.errors.password}</div>
+                          ) : null}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="pt-4">
+                      <Button
+                        label="Sign Up"
+                        type="submit"
+                        className="bg-blue-500 ml-2 hover:bg-blue-600"
+                      />
+                    </div>
+                    <div className="grid flex align-items-center justify-content-center pt-5">
+                      {" "}
+                      {/**Line Or Line */}
+                      <div className="col-5 ">
+                        <div
+                          style={{
+                            flex: 1,
+                            height: "1px",
+                            backgroundColor: "lightgrey",
+                          }}
+                        />
+                      </div>
+                      <div className="col-1 ">OR</div>
+                      <div className="col-5 ">
+                        <div
+                          style={{
+                            flex: 1,
+                            height: "1px",
+                            backgroundColor: "lightgrey",
+                          }}
+                        />
+                      </div>
+                    </div>
+                    {/**Line Or Line end*/}
+                  </div>
+                  <div className="pt-4">
+                    <Button
+                      label="Sign up with Google"
+                      className="w-full surface-0 ml-2 hover:surface-200 text-color border-400"
                     />
                   </div>
                   <div className="pt-4">
                     <Button
-                      label="Sign Up"
-                      className="bg-blue-500 ml-2 hover:bg-blue-600"
+                      label="Sign up with Facebook"
+                      className="w-full surface-0 ml-2 hover:surface-200 text-color border-400"
                     />
                   </div>
-                  <div className="grid flex align-items-center justify-content-center pt-5">
-                    {" "}
-                    {/**Line Or Line */}
-                    <div className="col-5 ">
-                      <div
-                        style={{
-                          flex: 1,
-                          height: "1px",
-                          backgroundColor: "lightgrey",
-                        }}
-                      />
-                    </div>
-                    <div className="col-1 ">OR</div>
-                    <div className="col-5 ">
-                      <div
-                        style={{
-                          flex: 1,
-                          height: "1px",
-                          backgroundColor: "lightgrey",
-                        }}
-                      />
-                    </div>
+                  <div className="text-left text-lg pt-3">
+                    Already have an account?{" "}
+                    <a className="text-blue-500 text-lg" href="../Login">
+                      {/*Sign up page connected*/}
+                      Log in
+                    </a>
                   </div>
-                  {/**Line Or Line end*/}
                 </div>
-                <div className="pt-4">
-                  <Button
-                    label="Sign up with Google"
-                    className="w-full surface-0 ml-2 hover:surface-200 text-color border-400"
-                  />
-                </div>
-                <div className="pt-4">
-                  <Button
-                    label="Sign up with Facebook"
-                    className="w-full surface-0 ml-2 hover:surface-200 text-color border-400"
-                  />
-                </div>
-                <div className="text-left text-lg pt-3">
-                  Already have an account?{" "}
-                  <a className="text-blue-500 text-lg" href="../Login">
-                    {/*Sign up page connected*/}
-                    Log in
-                  </a>
-                </div>
-              </div>
+              </form>
             </Card>
           </div>
           <div className="col-4 "></div>
