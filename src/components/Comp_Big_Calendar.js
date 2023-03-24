140ccbfbfb82507cee4b64eee85df9faaedb2123
Murat Barış Erol <58422636+bariserol761@users.noreply.github.com>
2023-03-24 22:40:55 +0300

    calender last db versions update

--- src/components/Comp_Big_Calendar.js
+++ src/components/Comp_Big_Calendar.js
@@ -6,11 +6,11 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Dialog } from "primereact/dialog";
 
 const localizer = momentLocalizer(moment);
-
+var fetchedItems = [];
 const Bigcalendar = () => {
-  const [events, setEvents] = useState([]);
+  const [event, setEvents] = useState([]);
   const [visible, setVisible] = useState(false);
-
+  const events = [];
   const promiseThen = new Promise((resolve, reject) => {
     fetch("http://localhost:3001/createMeeting/all")
       .then((response) => resolve(response.json()))
@@ -19,10 +19,12 @@ const Bigcalendar = () => {
   })
     .then((val) =>
       val.eventData.forEach((element) => {
-        console.log(element._id);
+        //console.log(element._id);
+        fetchedItems.push(element);
       })
     )
     .catch((rej) => console.log(rej));
+
   /*useEffect(() => {
     // Fetch events data from database
     fetch("http://localhost:3001/createMeeting/all")
@@ -32,7 +34,22 @@ const Bigcalendar = () => {
   }, []);*/
 
   const now = new Date();
+  if (fetchedItems.length > 0) {
+    for (var i = 0; i < fetchedItems.length; i++) {
+      console.log(fetchedItems[0]);
 
+      console.log("events: " + events);
+      events.push({
+        id: fetchedItems[i]._id,
+        title: fetchedItems[i].eventDescription,
+        allDay: false,
+        start: fetchedItems[i].eventStartDate,
+        end: fetchedItems[i].eventEndDate,
+      });
+      // console.dir("events: " + setEvents);
+    }
+  }
+  console.log(events);
   const showDialog = () => {
     setVisible(true);
   };
