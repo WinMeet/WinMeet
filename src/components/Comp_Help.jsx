8d2fc9a5e72644d3092e3b092cb7f4bdfecb204e
Murat Barış Erol <58422636+bariserol761@users.noreply.github.com>
2023-05-10 19:22:47 +0300

    help edit

--- src/components/Comp_Help.jsx
+++ src/components/Comp_Help.jsx
@@ -11,6 +11,37 @@ const CompHelp = () => {
       <div className="grid">
         <div className="col-10 col-offset-1">
           <AuthenticatedNavbar></AuthenticatedNavbar>
+          <div className="grid col-offset-1">
+            <div className="grid col-6 ">
+              <div className="text-3xl ">
+                <p className="font-bold"> How To Create Meeting </p>
+                <p className="text-lg">
+                  {" "}
+                  what you need to create a new meeting. First of all, you need
+                  to be on the home screen. Then press the "+" (plus) icon at
+                  the bottom right of the page.After that, a screen will come up
+                  to welcome you. Fill in the required sections on this screen
+                  (screen shown on the right).
+                </p>
+                <p className="font-bold"> Accelerate your sales cycle</p>
+                <p className="text-lg">
+                  {" "}
+                  Maintain deal momentum and eliminate scheduling friction at
+                  all stages of your sales cycle.
+                </p>
+                <p className="font-bold"> Close more transactions </p>
+                <p className="text-lg">
+                  {" "}
+                  Tailor reminder and follow-up sequences to drive transactions
+                  along, link with sales tools, and eliminate logistical
+                  responsibilities so you can focus on selling.
+                </p>
+              </div>
+            </div>
+            <div className="grid col-6 ">
+              <p>ajdnkajndkas</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
