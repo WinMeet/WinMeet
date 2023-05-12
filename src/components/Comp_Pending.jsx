0f24ecd2a31506078ef1f4444fab93a96dd7079a
cem256 <cemo99@gmail.com>
2023-05-12 22:21:27 +0300

    reload page

--- src/components/Comp_Pending.jsx
+++ src/components/Comp_Pending.jsx
@@ -29,7 +29,9 @@ const CompPending = () => {
         }),
       }
     )
-      .then((response) => {})
+      .then((response) => {
+        window.location.reload();
+      })
       .catch((error) => {
         console.error("Error:", error);
         // Handle the error
